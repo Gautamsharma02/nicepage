f47ee12f361d3a58dc6ff806c7290a34e10e10e6
Gautam sharma <88236442+Gautamsharma02@users.noreply.github.com>
2021-07-31 18:06:57 +0530

    Add files via upload

---
+++ nicepage/jss/app.js
@@ -0,0 +1,23 @@
+const toggleBtn =document.querySelector('.sidebar-toggle');
+const closeBtn =document.querySelector('.close-btn');
+const sidebar =document.querySelector('.sidebar');
+
+toggleBtn.addEventListener('click', function(){
+    // using add and remove class
+    
+    /*
+    if(sidebar.classList.contains('show-sidebar')){
+        sidebar.classList.remove('show-sidebar');
+    }else{
+        sidebar.classList.add('.show-sidebar');
+    }
+    */
+
+    //using toggle
+    sidebar.classList.toggle('show-sidebar')
+    sidebar.classList.toggle("back");
+});
+
+closeBtn.addEventListener('click', function(){
+    sidebar.classList.remove('show-sidebar');
+});
